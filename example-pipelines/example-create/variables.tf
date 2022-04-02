e577e119bc76d2ce54053cc2557e5de07f80fafd
gksstxx <101635831+gksstxx@users.noreply.github.com>
2022-04-02 22:58:27 +0900

    Update variables.tf

--- example-pipelines/example-create/variables.tf
+++ example-pipelines/example-create/variables.tf
@@ -30,13 +30,13 @@ variable "environment" {
 variable "subnet1_region" {
   type        = string
   description = "GCP Region where first subnet will be created"
-  default     = "us-central1"
+  default     = "asia-northeast1"
 }
 
 variable "subnet1_zone" {
   type        = string
   description = "GCP Zone within Subnet1 Region where GCE instance will be created"
-  default     = "us-central1-a"
+  default     = "asia-northeast1-a"
 }
 
 variable "subnet1_cidr" {
